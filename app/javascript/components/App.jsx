3221d6c59c9d50c44776636c8aa9c4e111bb363c
Giorgenes Gelatti <giorgenes@gmail.com>
2021-11-01 14:17:58 +1100

    Adds tailwind config

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const App = (props) => {
-  return <b>Olá App</b>;
-};
+const App = (props) => (
+  <React.Fragment>
+    <div className="max-w-lg mx-auto mt-16 text-center max-w-sm rounded overflow-hidden shadow-lg p-10">
+      <h1 className="mb-4 text-3xl font-black">Here are all our users!</h1>
+      <p className="text-lg leading-snug">If this looks nice, it means Tailwind is set up properly.</p>
+    </div>
+    <b>Olá App</b>;
+  </React.Fragment>
+);
 
 export default App;
 
